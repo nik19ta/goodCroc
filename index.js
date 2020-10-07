67f7468bcdc2c5d3cf17c554d14e412a6902e5cd
nik19ta <nikhvatov19@gmail.com>
2020-10-07 16:32:00 +0300

    deow in img

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const workbook = XLSX.readFile('Эко_боброшоп.xlsx');
 const sheet_name_list = workbook.SheetNames;
 const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(1654, 2339)
+const ctx = canvas.getContext('2d')
+
 let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
@@ -45,12 +49,30 @@ function send(goods, name, email, file) {
 
 for (let index = 0; index < xlData.length; index++) {
 
-    
     send(
         xlData[index].goodName,
         `${xlData[index].userLastName} ${xlData[index].userFirstName}`, 
         `${xlData[index].userName}@croc.ru` , 
         'file...'
         )
-    }
+}
     
+function createImg() {
+    loadImage('./croc.jpg').then((image) => {
+        ctx.drawImage(image, 0, 0, 1654, 2339)
+        ctx.font = 'bold 92px Croc'
+        ctx.rotate(0)
+        ctx.fillText(`Nikita Khvatov`, 135, 1150)
+        fs.writeFileSync('index.html', `<!DOCTYPE html >
+        <html lang="en">
+        <head>
+        <title>Document</title>
+        </head>
+        <body>
+        <img src="${canvas.toDataURL()}" />
+        </body >
+        </html >`)
+    })
+}
+
+createImg()
